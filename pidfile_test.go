dda6b113136d97e20303227c139cb24fd8dfc76b
chg1f <chongiofai@gmail.com>
2023-03-27 13:59:05 +0800

    test: update

--- pidfile_test.go
+++ pidfile_test.go
@@ -1,131 +1,29 @@
 package pidfile
 
 import (
-	"errors"
-	"io"
 	"os"
 	"path/filepath"
-	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPIDFile(t *testing.T) {
-	path := filepath.Join(os.TempDir(), "example/pidfile")
-	c, err := New(path)
-	if err != nil {
-		t.Error(err)
-	}
-	err = c.Generate()
-	if err != nil {
-		t.Error(err)
-	}
-	f, err := os.Open(path)
-	if err != nil {
-		t.Error(err)
-	}
-	bs, err := io.ReadAll(f)
-	if err != nil {
-		t.Error(err)
-	}
-	if string(bs) != strconv.Itoa(os.Getpid()) {
-		t.Error(string(bs) + "!=" + strconv.Itoa(os.Getpid()))
-	}
-	f.Close()
-	err = c.Cleanup()
-	if err != nil {
-		t.Error(err)
-	}
-	if _, err := os.Stat(path); !errors.Is(err, os.ErrNotExist) {
-		t.Error(os.ErrExist)
-	}
-}
-func TestPIDFileExist(t *testing.T) {
-	path := filepath.Join(os.TempDir(), "pidfile")
-	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		t.Error(err)
-	}
-	f.WriteString("0")
-	f.Close()
-	defer os.Remove(path)
-	c, err := New(path)
-	if err != nil {
-		t.Error(err)
-	}
-	err = c.Generate()
-	if !errors.Is(err, ErrFileExists) {
-		t.Error("overwrite")
-	}
-	f, err = os.Open(path)
-	if err != nil {
-		t.Error(err)
-	}
-	defer f.Close()
-	bs, err := io.ReadAll(f)
-	if err != nil {
-		t.Error(err)
-	}
-	if string(bs) != "0" {
-		t.Error("overwrited " + string(bs))
-	}
-}
-func TestPIDFileDuplicated(t *testing.T) {
-	path := filepath.Join(os.TempDir(), "pidfile")
-	c, err := New(path)
-	if err != nil {
-		t.Error(err)
-	}
-	err = c.Generate()
-	if err != nil {
-		t.Error(err)
-	}
-	defer c.Cleanup()
-	err = c.Generate()
-	if !errors.Is(err, ErrDuplicated) {
-		t.Error("duplicated")
-	}
+	path := filepath.Join(os.TempDir(), "test.pid")
+	pf, err := New(path)
+	assert.NoError(t, err)
+	assert.NotNil(t, pf)
+	assert.FileExists(t, path)
+	pf.Cleanup()
+	assert.NoFileExists(t, path)
 }
 
 func ExamplePIDFile() {
-	pf, err := New("./pidfile")
+	pf, err := New("./test.pid")
 	if err != nil {
-		panic(err)
-	}
-	if err := pf.Generate(); err != nil {
 		panic(err)
 	}
 	defer pf.Cleanup()
-	// ...
-
-	// Output:
-}
-
-func TestGenerate(t *testing.T) {
-	path := filepath.Join(os.TempDir(), "example/pidfile")
-	c := Generate(path)
-	f, err := os.Open(path)
-	if err != nil {
-		t.Error(err)
-	}
-	bs, err := io.ReadAll(f)
-	if err != nil {
-		t.Error(err)
-	}
-	if string(bs) != strconv.Itoa(os.Getpid()) {
-		t.Error(string(bs) + "!=" + strconv.Itoa(os.Getpid()))
-	}
-	f.Close()
-	err = c.Cleanup()
-	if err != nil {
-		t.Error(err)
-	}
-	if _, err := os.Stat(path); !errors.Is(err, os.ErrNotExist) {
-		t.Error(os.ErrExist)
-	}
-}
-
-func ExampleGenerate() {
-	defer Generate("./pidfile").Cleanup()
 	// ...
 
 	// Output:
